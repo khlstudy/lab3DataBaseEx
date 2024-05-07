838d336df8c6859774805bedc6395951988fa9ef
khlstudy <145128666+nayavua@users.noreply.github.com>
2024-05-07 23:51:08 +0300

    Add project files.

---
+++ lab3DataBaseEx/DBUtils.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace lab3DataBaseEx
+{
+    internal class DBUtils
+    {
+        public static MySqlConnection GetDBConnection()
+        {
+            string host = "localhost";
+            int port = 3306;
+            string database = "world";
+            string username = "monty";
+            string password = "some_pass";
+
+            return DBMySQLUtils.GetDBConnection(host, port, database, username, password);  
+        }
+    }
+}
---
+++ lab3DataBaseEx/Program.cs
@@ -0,0 +1,66 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3DataBaseEx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Getting Connection...");
+            MySqlConnection conn = DBUtils.GetDBConnection();
+
+            try
+            {
+                Console.WriteLine("Openning Connection...");
+
+                conn.Open();
+
+                Console.WriteLine("Connection successful!");
+                QueryEmploye(conn);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error:" + e.Message);
+            }
+            finally
+            {
+                conn.Close();
+                conn.Dispose();
+            }
+            Console.ReadLine();
+        }
+
+        private static void QueryEmploye(MySqlConnection conn)
+        {
+            string id, name, countryCode;
+            string sql = "SELECT Id, Name, CountryCode FROM city";
+
+            MySqlCommand cmd = new MySqlCommand();
+
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+
+            using (DbDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        id = reader["Id"].ToString();
+                        name = reader["Name"].ToString();
+                        countryCode = reader["CountryCode"].ToString();
+                        Console.WriteLine("-----------------------------------------");
+                        Console.WriteLine("Код: " + id+"Назва: " + name+"Код країни: " + countryCode);
+                        Console.WriteLine("-----------------------------------------");
+                    }
+                }
+            }
+        }
+    }
+}
